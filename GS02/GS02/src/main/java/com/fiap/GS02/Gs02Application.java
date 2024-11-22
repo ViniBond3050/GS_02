80db4f287a89f3c173f68c9cd6dd06444556a701
ViniBond3050 <116038607+ViniBond3050@users.noreply.github.com>
2024-11-22 20:45:31 -0300

    Add files via upload

---
+++ GS02/GS02/src/main/java/com/fiap/GS02/Gs02Application.java
@@ -0,0 +1,13 @@
+package com.fiap.GS02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gs02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Gs02Application.class, args);
+	}
+
+}
